529ddbaf11a2e33e3076b41020c57e5b769a15db
tibbi <tibor@kaputa.sk>
2019-01-19 16:14:38 +0100

    adding kotlin

--- library/src/main/java/com/alexvasilkov/gestures/views/interfaces/ClipView.java
+++
@@ -1,9 +0,0 @@
-package com.alexvasilkov.gestures.views.interfaces;
-
-import android.graphics.RectF;
-
-import androidx.annotation.Nullable;
-
-public interface ClipView {
-    void clipView(@Nullable RectF rect, float rotation);
-}
---
+++ library/src/main/kotlin/com/alexvasilkov/gestures/views/interfaces/ClipView.kt
@@ -0,0 +1,7 @@
+package com.alexvasilkov.gestures.views.interfaces
+
+import android.graphics.RectF
+
+interface ClipView {
+    fun clipView(rect: RectF?, rotation: Float)
+}
